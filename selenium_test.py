0ddd804882ce607e36ff801c47d817927668237f
seonghtun <ysh970718@gmail.com>
2023-07-14 18:05:25 +0900

    Feat selenium code

--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('user-agent={0}'.format(user_agent))
-driver = webdriver.Chrome('./chromedriver',options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(options=chrome_options)
 # 아무런 경고가 없다면 이상없이 작동되는 것입니다
 
-driver.get(url='https://naver.com')
+driver.get(url='https://yeyak.seoul.go.kr/web/search/selectPageListDetailSearchImg.do?code=T500&dCode=T502')
 # 페이지 이동
 
+menus = driver.find_elements(By.CSS_SELECTOR,'.img_board > li > a')
+list(menus)[0].click()
 print(driver.current_url)
+# print(driver.page_source)
+driver.back()
 # 이동되었는지 확인
+
+time.sleep(2)
 
 driver.close()
 # 종료
