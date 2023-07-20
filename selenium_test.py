af6bf305b2cb4d3ffb30da1f0e5b37043515ceb4
seonghtun <ysh970718@gmail.com>
2023-07-20 14:49:18 +0900

    add requirements.txt file and Featx selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 from bs4 import BeautifulSoup
+import math
+
+math.factorial(100000)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=chrome_options)
 # 아무런 경고가 없다면 이상없이 작동되는 것입니다
@@ -13,13 +19,20 @@ driver.get(url='https://yeyak.seoul.go.kr/web/search/selectPageListDetailSearchI
 # 페이지 이동
 
 menus = driver.find_elements(By.CSS_SELECTOR,'.img_board > li > a')
-list(menus)[0].click()
-print(driver.current_url)
-# print(driver.page_source)
-driver.back()
-# 이동되었는지 확인
+start = time.time()
+# for menu in menus:
 
-time.sleep(2)
+urls = []
 
+
+next_btn = driver.find_element(By.CLASS_NAME, 'next')
+next_btn.click()
+
+time.sleep(1)
+
+
+end = time.time()
+
+print(f"{end - start:.5f} sec")
 driver.close()
 # 종료
